52632e222598c66ec5170938fea8e1a8ff74ecf4
Richard W <49697500+riwi2802@users.noreply.github.com>
2021-06-14 23:19:44 +0000

    part one, first question & feedback

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,15 +15,20 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
-}
+ let candidateName = input.question("What is your name? : ")
+ console.log("Hello ", candidateName, "!")
+};
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  let candidateAnswer = input.question("Who was the first American woman in space? : ")
+    if (candidateAnswer === correctAnswer ) {
+      console.log("Congrats! This is correct.")
+    } else {
+        console.log("Sorry. This is not correct. The correct answer was Sally Ride.");
+      }
 }
-
+//sally ride is not correct but Sally Ride is, how to i change this R
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
