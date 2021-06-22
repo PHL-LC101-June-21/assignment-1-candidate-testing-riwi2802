9d54962ffdac97d9361f242d4b8d0cec681212cf
Richard W <49697500+riwi2802@users.noreply.github.com>
2021-06-22 00:45:12 +0000

    very messy

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,16 +8,12 @@ let candidateName = " ";
 let question = "Who was the first American woman in space?";
 let correctAnswer = "sally ride";
 let candidateAnswer = " ";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American Woman in space? : ", "True or false: 5 kilometer == 5000 meters? : ", "(5 + 3)/2 * 10 = ? : ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? : ", "What is the minimum crew size for the ISS : "
+];
+let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
+let candidateAnswers = [];
+let questionNumber = [1, 2, 3, 4, 5]
 
-
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
- let candidateName = input.question("What is your name? : ")
- console.log("Hello ", candidateName, "!")
-};
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -28,7 +24,23 @@ function askQuestion() {
         console.log("Sorry. This is not correct. The correct answer was Sally Ride.");
       }
 }
-\
+
+
+
+
+
+
+
+let i = 0;
+
+for (let i = 0; i < questions.length; i++) { 
+  candidateAnswers[i] = input.question(questions[i]);
+}
+for (let i = 0; i < questions.length; i++) { 
+  console.log(`You responded with ${candidateAnswers[i]} for question number ${questionNumber[i]}`)
+}
+
+
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
@@ -40,11 +52,23 @@ function gradeQuiz(candidateAnswers) {
   return grade;
 }
 
+
+
+
+
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
+  function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+ let candidateName = input.question("What is your name? : ")
+ console.log("Hello ", candidateName, "!")
+};
+
+
+
+  //askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
